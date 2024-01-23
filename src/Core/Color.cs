d3b5ea699ff3a96ba604c050f623421a7024c517
hox8 <corvoa89@gmail.com>
2024-01-23 13:07:51 +1300

    Implement UnrealScript types

---
+++ src/Core/Color.cs
@@ -0,0 +1,43 @@
+﻿namespace UnrealLib.Core;
+
+public record struct Color
+{
+    public byte B, G, R, A;
+
+    public Color(byte b, byte g, byte r, byte a)
+    {
+        B = b;
+        G = g;
+        R = r;
+        A = a;
+    }
+
+    public Color(byte bgra)
+    {
+        B = bgra;
+        G = bgra;
+        R = bgra;
+        A = bgra;
+    }
+}
+
+public record struct LinearColor
+{
+    public float R, G, B, A;
+
+    public LinearColor(float rgba)
+    {
+        B = rgba;
+        G = rgba;
+        R = rgba;
+        A = rgba;
+    }
+
+    public LinearColor(float r, float g, float b, float a)
+    {
+        G = g;
+        R = r;
+        B = b;
+        A = a;
+    }
+}
---
+++ src/Core/Vector.cs
@@ -0,0 +1,58 @@
+﻿namespace UnrealLib.Core;
+
+public record struct Vector2
+{
+    public float X, Y;
+
+    public Vector2(float x, float y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public Vector2(float xy)
+    {
+        X = xy;
+        Y = xy;
+    }
+}
+
+public record struct Vector3
+{
+    public float X, Y, Z;
+
+    public Vector3(float x, float y, float z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+    public Vector3(float xyz)
+    {
+        X = xyz;
+        Y = xyz;
+        Z = xyz;
+    }
+}
+
+public record struct Vector4
+{
+    public float X, Y, Z, W;
+
+    public Vector4(float x, float y, float z, float w)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+        W = w;
+    }
+
+    public Vector4(float xyzw)
+    {
+        X = xyzw;
+        Y = xyzw;
+        Z = xyzw;
+        W = xyzw;
+    }
+}
