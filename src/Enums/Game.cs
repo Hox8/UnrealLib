58ce46eb4d24f3df95960c3ec419ac947431d024
hox8 <corvoa89@gmail.com>
2024-01-27 14:32:55 +1300

    Add 'Unspecified' member to Game enum

--- src/Enums/Game.cs
+++ src/Enums/Game.cs
@@ -5,11 +5,14 @@ namespace UnrealLib.Enums;
 [Flags]
 public enum Game : byte
 {
+    Unspecified = 0,
+    Unknown = 1 << 0,
+
     // Coalesced decryption depends on this specific order
-    Unknown = 0,
     IB1 = 1 << 1,
     IB2 = 1 << 2,
     IB3 = 1 << 3,
     Vote = 1 << 4,
+
     All = Vote | IB1 | IB2 | IB3
 }
